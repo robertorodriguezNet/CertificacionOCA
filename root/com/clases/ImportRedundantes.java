c2db8bf99b2e89e105ee5b7462e6a570f5d70851
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2025-05-03 20:24:29 +0200

    Added new file

---
+++ root/com/clases/ImportRedundantes.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.lang.*;
+import java.util.Random;
+import java.util.*;
+
+public class ImportRedundantes{
+
+	public static void manin(String[] args){
+		
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+	}
+
+}	
